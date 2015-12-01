5fec43de6741b97c0ec43dec3837791e21379bd8
schung <schung@nuvoton.com>
2015-12-01 13:30:27 +0800

    Add Python-2.7.9
    Change-Id: Ib286dc9a8becf91037c811bf12b9f304d4b340be

---
+++ Python-2.7.9/make_python.sh
@@ -0,0 +1,17 @@
+
+if test "$(arch)" = i686 ; then OS=i686-pc-linux-gnu; fi;
+if test "$(arch)" = x86_64 ; then OS=x86_64-unknown-linux-gnu; fi;
+
+(cp Makefile.pre.in_org Makefile.pre.in && \
+./configure && \
+make python Parser/pgen && \
+mv python hostpython && \
+mv Parser/pgen Parser/hostpgen && \
+make distclean && \
+patch -p1 < $PWD/Python-2.7.9-xcompile-patch && \
+ac_cv_file__dev_ptc=no ac_cv_file__dev_ptmx=no ./configure --host=arm-linux --build=$OS --disable-ipv6 --prefix=$PWD/_install && \
+patch -p0 < $PWD/Python-2.7.9-removetest-patch && \
+make -j2 HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen && \
+make -j2 install HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen \
+)
+
