d3968aa146bff20dbfc1489b88de515b8ca0de53
schung <schung@nuvoton.com>
2016-02-02 14:38:47 +0800

    copy python libray to install directory
    Change-Id: Iec746f36e652c930ace7d8b6f69c5af8b4a9f49b

--- Python-2.7.9/make_python.sh
+++ Python-2.7.9/make_python.sh
@@ -9,9 +9,10 @@ mv python hostpython && \
 mv Parser/pgen Parser/hostpgen && \
 make distclean && \
 patch -p1 < $PWD/Python-2.7.9-xcompile-patch && \
-ac_cv_file__dev_ptc=no ac_cv_file__dev_ptmx=no ./configure --host=arm-linux --build=$OS --disable-ipv6 --prefix=$PWD/_install && \
+ac_cv_file__dev_ptc=no ac_cv_file__dev_ptmx=no ./configure --host=arm-linux --build=$OS --disable-ipv6 --enable-shared --prefix=$PWD/_install && \
 patch -p0 < $PWD/Python-2.7.9-removetest-patch && \
 make -j2 HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen && \
-make -j2 install HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen \
+make -j2 install HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen &&\
+cp $PWD/build/lib.linux2-arm-2.7/*.so $PWD/_install/lib/python2.7/lib-dynload \
 )
 
