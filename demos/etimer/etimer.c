10bfd44ea09c40ee238cbab2dfc759100718ce84
cfli0 <cfli0@nuvoton.com>
2016-08-10 19:09:29 +0800

    Modify application for ETIMER Add ETIMER wakeup, periodic mode and free counting mode testing.
    Change-Id: I8a4533aebde03aa1a7282dafa110b4b71f5c19c6

--- demos/etimer/etimer.c
+++ demos/etimer/etimer.c
@@ -26,29 +26,84 @@ int main(int argc, char **argv)
 	int fd[4], i, j, mode = ETMR_CAP_EDGE_FF;
 	fd_set rfd;
 	struct timeval tv;
-	unsigned int cap;
+	unsigned int cap, cnt;
 	int ret;
 	char dev[4][14] = {"/dev/etimer0",
-			"/dev/etimer1",
-			"/dev/etimer2",
-			"/dev/etimer3"};
-	int period[4] = {1000, 800, 600, 400};	// Priod (in us) of 4 timer channels
-
+                       "/dev/etimer1",
+                       "/dev/etimer2",
+                       "/dev/etimer3"};
+	int period[4] = {1000, 800, 600, 400};	// Period (in us) of 4 timer channels
+    int wkperiod[4] = {16384, 32768, 65536, 163840};    
+    int timeout = 1000000;  // 1 sec
+    
+    for(i = 0; i < 4; i++) {        
+        fd[i] = open(&dev[i][0], O_RDWR);
+        if(fd[i] < 0)
+            printf("open etimer %d error\n", i); 
+            
+        printf("Channel %d Time-out Wakeup testing\n", i);
+        // switch clock, power down mode using 32kHz            
+        ioctl(fd[i], ETMR_IOC_CLKLXT, NULL);
+        
+        ret = ioctl(fd[i], ETMR_IOC_PERIODIC_FOR_WKUP, &wkperiod[i]);
+        if(ret == -1)
+            printf("clock source error\n");   
+        else    
+        {
+            system("echo mem > /sys/power/state");            
+            printf("Wake up from ETimer%d.\n",i);
+            printf("hit any key to test next channel\n");
+            getchar();
+            ioctl(fd[i], ETMR_IOC_STOP, NULL);
+        }
+    }
+    printf("hit any key to quit periodic wake-up mode\n");
+    getchar();
+    
+    //switch clock 12MHz
+    for(i = 0; i < 4; i++) {        
+        ioctl(fd[i], ETMR_IOC_CLKHXT, NULL);
+    }
+    
+    //Periodic demo
+    for(i = 0; i < 4; i++) {
+        printf("Channel %d Periodic testing\n", i);    
+        ioctl(fd[i], ETMR_IOC_PERIODIC, &timeout);        
+        for(j = 0; j < 5; j++) {            
+            read(fd[i], &cnt, sizeof(cnt));
+            printf("%d sec\n", cnt);
+		}
+        ioctl(fd[i], ETMR_IOC_STOP, NULL);
+    }
+    printf("hit any key to quit periodic mode\n");
+    getchar();    
 	// Toggle output demo
 	for(i = 0; i < 4; i++) {
-		fd[i] = open(&dev[i][0], O_RDWR);
-		if(fd[i] < 0)
-			printf("open etimer %d error\n", i);
 		ioctl(fd[i], ETMR_IOC_TOGGLE, &period[i]);
 	}
 
-	printf("hit any key to quit toggle mode\n");
+    printf("hit any key to quit toggle mode\n");
+	getchar();
+	// Free counting mode, block
+	for(i = 0; i < 4; i++) {
+		printf("Channel %d capture testing\n", i);
+		ioctl(fd[i], ETMR_IOC_FREE_COUNTING, &mode);
+		for(j = 0; j < 10; j++) {
+			read(fd[i], &cap, sizeof(cap));
+			printf("cap:%d Hz\n", cap);
+		}
+		printf("hit any key to test next channel\n");
+		getchar();
+		ioctl(fd[i], ETMR_IOC_STOP, NULL);
+	}
+    
+	printf("hit any key to quit free counting mode\n");
 	getchar();
 	// Trigger counting mode, block
 	for(i = 0; i < 4; i++) {
 		printf("Channel %d capture testing\n", i);
 		ioctl(fd[i], ETMR_IOC_TRIGGER_COUNTING, &mode);
-		for(j = 0; j < 100; j++) {
+		for(j = 0; j < 10; j++) {
 			read(fd[i], &cap, sizeof(cap));
 			printf("cap:%d us\n", cap);
 		}
@@ -65,7 +120,7 @@ int main(int argc, char **argv)
 		printf("Channel %d capture poll testing\n", i);
 		ioctl(fd[i], ETMR_IOC_TRIGGER_COUNTING, &mode);
 
-		for(j = 0; j < 100; j++) {
+		for(j = 0; j < 10; j++) {
 
 			ret = select(fd[i] + 1, &rfd, NULL, NULL, NULL);
 			if(ret == -1) {
