2c2254db8ff3c3253aa8e6304f6c788e3987f345
csyang2 <csyang2@nuvoton.com>
2019-12-03 19:55:46 +0800

    Update emWin lib from V5.48k2 to V5.48k4 for adding new 2d fill rectangle in rgb565
    Change-Id: I29a11c7eebf3acbceb72813392da8eda342d20c9

--- emWin/Include/GUI_Version.h
+++ emWin/Include/GUI_Version.h
@@ -44,7 +44,7 @@ Purpose     : Include file defining current GUI version
 #ifndef  GUI_VERSION_H
 #define  GUI_VERSION_H
 
-#define GUI_VERSION 548112
+#define GUI_VERSION 548114
 
 #endif   /* Avoid multiple inclusion */
 
