d5fcab14b777c76e84b727d8501d1f686966bc8d
Jacky Huang <ychuang3@nuvoton.com>
2023-08-31 16:32:06 +0800

    Update emWin Library for NUC970 Linux-5.10 JPEG support
    In Linux-3.10, the device name of JPEG is 'video0', while in
    Linux-5.10, the device name becomes 'v4l-subdev0'.
    
    Update emWin library to try opening both device, thus it
    can work on both NUC970 Linux-3.10 and Linux-5.10.
    
    Change-Id: Iadd43e35818e7e024375f98ea0f04718ea7951a7

--- emWin/Include/GUI_Version.h
+++ emWin/Include/GUI_Version.h
@@ -44,7 +44,7 @@ Purpose     : Include file defining current GUI version
 #ifndef  GUI_VERSION_H
 #define  GUI_VERSION_H
 
-#define GUI_VERSION 610064
+#define GUI_VERSION 610065
 
 #endif   /* Avoid multiple inclusion */
 
